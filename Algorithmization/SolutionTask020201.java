b8f5d5ef5d879ab10de417c7eea8de09def60a04
Ziliboba <ziliboba.work@gmail.com>
2023-05-10 16:40:23 +0300

    add new solution

---
+++ Algorithmization/SolutionTask020201.java
@@ -0,0 +1,46 @@
+package by.epam.introduction_to_java.algorithmization;
+
+import java.util.Random;
+
+//Решаем Algorithmization - Задачи. Массивы массивов - задача #1
+public class SolutionTask020201 {
+
+	public static void main(String[] args) {
+		// Дана матрица
+		int[][] a = new int[10][10];
+
+		// Заполним её случайными значениеями
+		// Для этого используем класс Rendom который возвращает случайное положительное
+		// число
+		Random random = new Random();
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				a[i][j] = random.nextInt(100);
+			}
+		}
+
+		System.out.println("Изначально наша матрица имеет вид");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println();
+			for (int j = 0; j < a.length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+		}
+
+		System.out.println();
+
+		// Выведем на печать все нечётные столбцы у которых первый элемент больше
+		// последнего
+		System.out.println();
+		for (int j = 0; j < a[a.length - 1].length; j = j + 2) {
+			if (a[0][j] > a[a.length - 1][j]) {
+				System.out.println("столбец номер " + (j));
+				for (int i = 0; i < a.length; i++) {
+					System.out.println(a[i][j] + " ");
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Algorithmization/SolutionTask020202.java
@@ -0,0 +1,46 @@
+package by.epam.introduction_to_java.algorithmization;
+
+import java.util.Random;
+
+//Решаем Algorithmization - Задачи. Массивы массивов - задача #2
+public class SolutionTask020202 {
+
+	public static void main(String[] args) {
+		// Дана квадратная матрица
+		int[][] a = new int[10][10];
+
+		// Заполним её случайными значениеями
+		// Для этого используем класс Rendom который возвращает случайное положительное
+		// число
+		Random random = new Random();
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				a[i][j] = random.nextInt(100);
+			}
+		}
+
+		System.out.println("Изначально наша матрица имеет вид");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println();
+			for (int j = 0; j < a.length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+		}
+
+		System.out.println();
+		System.out.println("Диагональ из верхнего левого в нижний правый ");
+		// Выведем на экран элементы на диагоналях
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i][i]);
+		}
+
+		System.out.println();
+		System.out.println("Диагональ из нижнего левого в верхний правый ");
+		// Выведем на экран элементы на диагоналях
+		for (int i = a.length - 1, j = 0; i >= 0; i--, j++) {
+			System.out.println(a[i][j]);
+		}
+
+	}
+
+}
